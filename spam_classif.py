0da8a20f8ce5ec8a09e5336179d1b598461f7a52
norman000 <norman000@mail.ru>
2014-03-29 00:58:12 +0300

    Update spam_classif.py

--- spam_classif.py
+++ spam_classif.py
@@ -41,19 +41,23 @@ def train(data_tr, way_tr, data_mn, way_mn, povt_mass):
     vect_tr = []
     vect_mn = []
     while n < len(data_tr):
-          a = [0 for i in xrange(50)]
+          a = [0 for i in xrange(52)]
           a.append(data_tr[n]['is_spam'])
           vect_tr.append(a)
           tokens = nltk.word_tokenize(data_tr[n]['subject'])         
           while m < len(povt_mass):
                 vect_tr[n][m] = tokens.count(povt_mass[m])
                 m = m + 1
-          m = 0                
+          m = 0          
+          vect_tr[n][50] = (len(tokens)) / len(nltk.word_tokenize(data_tr[n]['body'])) 
+          for i in tokens:
+              if re.search("Re", i) == True:
+                 vect_tr[n][51] = 50 
           n = n + 1  
     n = 0 
     m = 0
     while n < len(data_mn):
-          a = [0 for i in xrange(50)]
+          a = [0 for i in xrange(52)]
           a.append(way_mn[n])
           vect_mn.append(a)
           tokens = nltk.word_tokenize(data_mn[n]['subject']) 
@@ -61,6 +65,10 @@ def train(data_tr, way_tr, data_mn, way_mn, povt_mass):
                 vect_mn[n][m] = tokens.count(povt_mass[m])
                 m = m + 1
           m = 0              
+          vect_mn[n][50] = (len(tokens)) / len(nltk.word_tokenize(data_mn[n]['body']))  
+          for i in tokens:
+              if re.search("Re", i) == True:
+                 vect_mn[n][51] = 50 
           n = n + 1  
     return vect_tr, vect_mn 
 
@@ -72,36 +80,32 @@ def classif(vect_tr, vect_mn):
     sum = 0
     long_mass = []
     while n < len(vect_mn):
+      #print n
       while m < len(vect_tr):
-        while c != 50:
+        while c != 52:
           koaf = ((vect_tr[m][c]-vect_mn[n][c])**2)
           sum = koaf + sum
           c = c + 1
         sqr = np.sqrt(sum)
         c = 0
-        long_mass.append([sqr, vect_tr[m][50]])
-        #
+        long_mass.append([sqr, vect_tr[m][52]])
         sum = 0
-        koaf = 0
         m = m + 1
-      print n
       long_mass = sorted(long_mass, key=itemgetter(0))
       vsp_mass = []
-      d = 0
-      while d < 20:
-        vsp_mass.append(long_mass[d][1])
-        d = d + 1
-      d = 0
+      k = 0
+      while k < 3:
+        vsp_mass.append(long_mass[k][1])
+        k = k + 1
+      k = 0
       if vsp_mass.count(False) >= vsp_mass.count(True):
         f.write(u'%s\t%s\n' % (way_mn[n], False))
       else:
         f.write(u'%s\t%s\n' % (way_mn[n], True))
       vsp_mass = []
       long_mass = []
-      sum = 0
       n = n + 1
-      m = 0
-    
+      m = 0  
     f.close()
 
 data_tr, way_tr = read_tr('/home/partizan/classif2/spam_data/train')
